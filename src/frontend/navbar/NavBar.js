b382862e4850a62a17b72f565279640ad2217c2e
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2022-12-14 21:16:17 +0530

    update

--- src/frontend/navbar/NavBar.js
+++
@@ -1,9 +0,0 @@
-import './nav.css'
-import rslogo from '../img/rslogo.png'
-export default function NavBar(){
-    return (
-        <div className='mainnav'>
-            <img className='mylogo' src={rslogo} alt="Rudhram.S" />
-        </div>
-    )
-}
